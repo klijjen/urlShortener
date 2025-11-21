7f9b8a9175bc90c65ac4b8f65bdd7c90dc04e2ec
lenalyudofa <m2309678@edu.misis.ru>
2025-11-21 16:30:42 +0300

    Создать интерфейс UrlRepository

---
+++ src/main/java/com/example/urlshortener/repository/UrlRepository.java
@@ -0,0 +1,14 @@
+package com.example.urlshortener.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UrlRepository extends JpaRepository<UrlEntity, UUID> {
+    Optional<UrlEntity> findByShortCode(String shortCode);
+    Optional<UrlEntity> findByOriginalUrl(String originalUrl);
+    boolean existsByShortCode(String shortCode);
+}
