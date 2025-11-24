773c7b2d124205ad26b8030aea74dc70965c44f1
lenalyudofa <m2309678@edu.misis.ru>
2025-11-24 16:49:37 +0300

    Удалить неиспользуемые поля и индекс

--- src/main/java/com/github/klijjen/urlshortener/repository/UrlEntity.java
+++ src/main/java/com/github/klijjen/urlshortener/repository/UrlEntity.java
@@ -1,17 +1,14 @@
 package com.github.klijjen.urlshortener.repository;
 
-import com.github.klijjen.urlshortener.Utils;
-
 import jakarta.persistence.*;
 
-import java.time.LocalDateTime;
 import java.util.*;
 
+import static com.github.klijjen.urlshortener.Utils.getRandomUUID;
+
+
 @Entity
-@Table(name = "urls", indexes = {
-        @Index(name = "idx_short_code", columnList = "shortCode"),
-        @Index(name = "idx_created_at", columnList = "createdAt")
-})
+@Table(name = "urls", indexes = {@Index(name = "idx_short_code", columnList = "shortCode"),})
 public class UrlEntity {
 
     @Id
@@ -23,15 +20,8 @@ public class UrlEntity {
     @Column(nullable = false, unique = true)
     private String shortCode;
 
-    @Column(nullable = false)
-    private LocalDateTime createdAt;
-
-    @Column
-    private LocalDateTime expiresAt;
-
     public UrlEntity() {
-        this.id = Utils.getRandomUUID();
-        this.createdAt = LocalDateTime.now();
+        this.id = getRandomUUID();
     }
 
     public UrlEntity(String originalUrl, String shortCode) {
@@ -40,23 +30,11 @@ public class UrlEntity {
         this.shortCode = shortCode;
     }
 
-    public UUID getId() {
-        return id;
-    }
-
     public String getOriginalUrl() {
         return originalUrl;
     }
 
     public String getShortCode() {
         return shortCode;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public LocalDateTime getExpiresAt() {
-        return expiresAt;
     }
 }
